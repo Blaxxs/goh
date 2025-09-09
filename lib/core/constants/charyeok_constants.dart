c1843ecc1240a64442f38e6997c71b2838085c80
Blaxxs <k-yul@naver.com>
2025-09-09 09:46:58 +0900

    Auto-commit on save: 2025-09-09 09:46:58

--- lib/core/constants/charyeok_constants.dart
+++ lib/core/constants/charyeok_constants.dart
@@ -8,14 +8,6 @@ class Charyeok {
 
 const List<Charyeok> charyeoks = [
   Charyeok(
-    name: '탐',
-    imagePath: 'assets/images/charyeoks/tam.png',
-    grades: [
-      {'grade': 1, 'effects': {'base_attack_increase': 160, 'skill_damage_increase': 30}},
-      {'grade': 2, 'effects': {'base_attack_increase': 200, 'skill_damage_increase': 40}},
-    ],
-  ),
-  Charyeok(
     name: '나봉침',
     imagePath: 'assets/images/charyeoks/nabongchim.png',
     grades: [
