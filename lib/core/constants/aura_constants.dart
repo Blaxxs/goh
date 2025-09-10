3a13b3e4ff9542a99d430e02dadc87c79ff23f9e
Blaxxs <k-yul@naver.com>
2025-09-10 09:23:20 +0900

    Auto-commit on save: 2025-09-10 09:23:20

--- lib/core/constants/aura_constants.dart
+++ lib/core/constants/aura_constants.dart
@@ -7,9 +7,6 @@ class Aura {
 }
 
 const List<Aura> auras = [
-  Aura(name: '공격력 오라', imagePath: 'assets/images/auras/attack_aura.png', effects: {'attack_power': 2000}),
-  Aura(name: '스킬 데미지 오라', imagePath: 'assets/images/auras/skill_damage_aura.png', effects: {'skill_damage_increase': 35}),
-  Aura(name: '공격력% 오라', imagePath: 'assets/images/auras/attack_percent_aura.png', effects: {'attack_power_increase': 10}),
   Aura(name: '증오', imagePath: 'assets/images/auras/hatred.png', effects: {'attack_power': 2000}),
   Aura(name: '마왕', imagePath: 'assets/images/auras/lucifer.png', effects: {'skill_damage_increase': 35}),
   Aura(name: '10주년 블루', imagePath: 'assets/images/auras/blue.png', effects: {'attack_power_increase': 10}),
