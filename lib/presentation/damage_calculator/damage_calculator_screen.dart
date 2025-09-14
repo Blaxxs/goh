526459cae1fe381cccd2a884333908267176b2f7
Blaxxs <k-yul@naver.com>
2025-09-14 09:42:00 +0900

    Auto-commit on save: 2025-09-14 09:42:00

--- lib/presentation/damage_calculator/damage_calculator_screen.dart
+++ lib/presentation/damage_calculator/damage_calculator_screen.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:goh_calculator/core/constants/box_constants.dart';
+import 'package:goh_calculator/core/services/settings_service.dart';
 import 'package:goh_calculator/core/widgets/app_drawer.dart';
 import 'package:goh_calculator/core/constants/damage_calculator_constants.dart';
 import 'package:goh_calculator/core/constants/charyeok_constants.dart';
@@ -8,6 +9,22 @@ import 'package:intl/intl.dart';
 
 enum RebirthRealm { none, heavenly, demon }
 enum RebirthStat { none, skillDamage, attackPower, critDamage, normalDamage }
+enum CrestType { none, attack, critDamage, skillDamage }
+
+class Crest {
+  final String name;
+  final CrestType type;
+  final IconData icon;
+
+  const Crest({required this.name, required this.type, required this.icon});
+}
+
+const List<Crest> crests = [
+  Crest(name: '선택 안함', type: CrestType.none, icon: Icons.cancel_outlined),
+  Crest(name: '공격강화 문장', type: CrestType.attack, icon: Icons.flash_on),
+  Crest(name: '치명피해 문장', type: CrestType.critDamage, icon: Icons.star),
+  Crest(name: '스킬 문장', type: CrestType.skillDamage, icon: Icons.school),
+];
 
 class DamageCalculatorScreen extends StatefulWidget {
   const DamageCalculatorScreen({super.key});
@@ -35,12 +52,13 @@ Color _getBorderColorForGrade(CharyeokGrade? grade) {
 }
 
 class _DamageCalculatorScreenState extends State<DamageCalculatorScreen> {
-  // Character, Charyeok, and Spirit
+  // Selections
   Character? _selectedCharacter;
   Charyeok? _selectedCharyeok;
   CharyeokGrade? _selectedCharyeokGrade;
   int _selectedCharyeokStar = 1;
   Spirit? _selectedSpirit;
+  Crest? _selectedCrest;
 
   // Rebirth
   RebirthRealm _selectedRebirthRealm = RebirthRealm.none;
@@ -55,11 +73,9 @@ class _DamageCalculatorScreenState extends State<DamageCalculatorScreen> {
   final _additionalAttackPowerController = TextEditingController();
   final _leaderEffectController = TextEditingController();
   final _highSchoolBuffController = TextEditingController();
-  final _moonBaseBuffController = TextEditingController();
   final _powerUpController = TextEditingController();
   final _critDamageController = TextEditingController();
   final _divineItemCritDamageController = TextEditingController();
-  final _crestCritDamageController = TextEditingController();
   final _accessoryNormalDamageController = TextEditingController();
   final _equipmentNormalDamageController = TextEditingController();
   final _divineItemNormalDamageController = TextEditingController();
@@ -73,16 +89,40 @@ class _DamageCalculatorScreenState extends State<DamageCalculatorScreen> {
   final _divineItemMinigameDamageController = TextEditingController();
   final _fragmentMinigameDamageController = TextEditingController();
   final _rebirthStatValueController = TextEditingController();
+  final _crestValueController = TextEditingController();
 
-  // --- Rebirth Bonuses ---
+  // --- Data Maps ---
   final List<int> demonRebirthAttackBonus = [0, 50, 50, 100, 100, 200, 200, 300, 300, 450];
   final List<int> heavenlyRebirthAttackBonus = [0, 50, 100, 200, 300, 450, 600, 800, 1000, 1400];
+  final Map<int, double> moonBaseAttackBuffs = {
+    0: 0, 1: 0.1, 2: 0.1, 3: 0.2, 4: 0.2, 5: 0.3, 6: 0.3, 7: 0.4, 8: 0.4, 9: 0.5, 10: 1.5,
+    11: 1.6, 12: 1.6, 13: 1.7, 14: 1.7, 15: 1.8, 16: 1.8, 17: 1.9, 18: 1.9, 19: 2, 20: 3,
+    21: 3.1, 22: 3.1, 23: 3.2, 24: 3.2, 25: 3.3, 26: 3.3, 27: 3.4, 28: 3.4, 29: 3.5, 30: 4.5,
+    31: 4.6, 32: 4.6, 33: 4.7, 34: 4.7, 35: 4.8, 36: 4.8, 37: 4.9, 38: 4.9, 39: 5, 40: 6,
+    41: 6.1, 42: 6.1, 43: 6.2, 44: 6.2, 45: 6.3, 46: 6.3, 47: 6.4, 48: 6.4, 49: 6.5, 50: 7.5,
+    51: 7.6, 52: 7.6, 53: 7.7, 54: 7.7, 55: 7.8, 56: 7.8, 57: 7.9, 58: 7.9, 59: 8, 60: 9,
+    61: 9.1, 62: 9.1, 63: 9.2, 64: 9.2, 65: 9.3, 66: 9.3, 67: 9.4, 68: 9.4, 69: 9.5, 70: 11.5,
+    71: 11.6, 72: 11.6, 73: 11.7, 74: 11.7, 75: 11.8, 76: 11.8, 77: 11.9, 78: 11.9, 79: 12, 80: 14,
+    81: 14.1, 82: 14.1, 83: 14.2, 84: 14.2, 85: 14.3, 86: 14.3, 87: 14.4, 88: 14.4, 89: 14.5, 90: 16.5,
+    91: 16.6, 92: 16.6, 93: 16.7, 94: 16.7, 95: 17.8, 96: 17.8, 97: 17.9, 98: 17.9, 99: 18, 100: 20,
+    101: 20.1, 102: 20.1, 103: 20.2, 104: 20.2, 105: 20.3, 106: 20.3, 107: 20.4, 108: 20.4, 109: 20.5, 110: 21.5,
+    111: 21.6, 112: 21.6, 113: 21.7, 114: 21.7, 115: 21.8, 116: 21.8, 117: 21.9, 118: 21.9, 119: 22, 120: 23,
+    121: 23.1, 122: 23.1, 123: 23.2, 124: 23.2, 125: 23.3, 126: 23.3, 127: 23.4, 128: 23.4, 129: 23.5, 130: 24.5,
+    131: 24.6, 132: 24.6, 133: 24.7, 134: 24.7, 135: 24.8, 136: 24.8, 137: 24.9, 138: 24.9, 139: 25, 140: 26,
+    141: 26.1, 142: 26.1, 143: 26.2, 144: 26.2, 145: 26.3, 146: 26.3, 147: 26.4, 148: 26.4, 149: 26.5, 150: 27.5,
+    151: 27.6, 152: 27.6, 153: 27.7, 154: 27.7, 155: 27.8, 156: 27.8, 157: 27.9, 158: 27.9, 159: 28, 160: 29,
+    161: 29.1, 162: 29.1, 163: 29.2, 164: 29.2, 165: 29.3, 166: 29.3, 167: 29.4, 168: 29.4, 169: 29.5, 170: 31.5,
+    171: 31.6, 172: 31.6, 173: 31.7, 174: 31.7, 175: 31.8, 176: 31.8, 177: 31.9, 178: 31.9, 179: 32, 180: 34,
+    181: 34.1, 182: 34.1, 183: 34.2, 184: 34.2, 185: 34.3, 186: 34.3, 187: 34.4, 188: 34.4, 189: 34.5, 190: 36.5,
+    191: 36.6, 192: 36.6, 193: 36.7, 194: 36.7, 195: 37.8, 196: 37.8, 197: 37.9, 198: 37.9, 199: 38, 200: 40,
+  };
 
   @override
   void initState() {
     super.initState();
     _selectedCharacter = characters[0];
     _selectedSpirit = spirits[0];
+    _selectedCrest = crests[0];
     if (charyeoks.isNotEmpty) {
       _selectedCharyeok = charyeoks[0];
       if (_selectedCharyeok!.availableGrades.isNotEmpty) {
@@ -96,11 +136,9 @@ class _DamageCalculatorScreenState extends State<DamageCalculatorScreen> {
     _additionalAttackPowerController.dispose();
     _leaderEffectController.dispose();
     _highSchoolBuffController.dispose();
-    _moonBaseBuffController.dispose();
     _powerUpController.dispose();
     _critDamageController.dispose();
     _divineItemCritDamageController.dispose();
-    _crestCritDamageController.dispose();
     _accessoryNormalDamageController.dispose();
     _equipmentNormalDamageController.dispose();
     _divineItemNormalDamageController.dispose();
@@ -114,6 +152,7 @@ class _DamageCalculatorScreenState extends State<DamageCalculatorScreen> {
     _divineItemMinigameDamageController.dispose();
     _fragmentMinigameDamageController.dispose();
     _rebirthStatValueController.dispose();
+    _crestValueController.dispose();
     super.dispose();
   }
 
@@ -199,6 +238,27 @@ class _DamageCalculatorScreenState extends State<DamageCalculatorScreen> {
         }
     }
 
+    // --- Crest Bonus ---
+    double crestValue = _getParser(_crestValueController);
+    double crestAttackBuff = 1.0;
+    double crestCritDamage = 0;
+    double crestSkillDamage = 0;
+    if (_selectedCrest != null) {
+        switch(_selectedCrest!.type) {
+            case CrestType.attack:
+                crestAttackBuff = 1 + (crestValue / 100);
+                break;
+            case CrestType.critDamage:
+                crestCritDamage = crestValue;
+                break;
+            case CrestType.skillDamage:
+                crestSkillDamage = crestValue;
+                break;
+            case CrestType.none:
+                break;
+        }
+    }
+
     // --- Part 1: Base Attack Calculation ---
     double baseAttack = _selectedCharacter!.baseAttackPower.toDouble();
     double additionalAttack = _getParser(_additionalAttackPowerController);
@@ -212,17 +272,19 @@ class _DamageCalculatorScreenState extends State<DamageCalculatorScreen> {
     double totalBaseAttack = (baseAttack * charyeokBaseAttackIncrease) + additionalAttack + rebirthAttackBonus + rebirthAttackOption;
 
     // --- Part 2: Multipliers ---
+    final int dalgijiLevel = SettingsService.instance.stageSettings.dalgijiLevel ?? 0;
+    final double moonBaseAttackPercent = moonBaseAttackBuffs[dalgijiLevel] ?? 0.0;
+    double moonBaseBuff = 1 + (moonBaseAttackPercent / 100);
+
     double leaderBuff = _getParser(_leaderEffectController) / 100;
     double highSchoolBuff = _getParser(_highSchoolBuffController) / 100;
-    double moonBaseBuff = _getParser(_moonBaseBuffController) / 100;
     double powerUpBuff = _getParser(_powerUpController) / 100;
 
     leaderBuff = leaderBuff == 0 ? 1.0 : leaderBuff;
     highSchoolBuff = highSchoolBuff == 0 ? 1.0 : highSchoolBuff;
-    moonBaseBuff = moonBaseBuff == 0 ? 1.0 : moonBaseBuff;
     powerUpBuff = powerUpBuff == 0 ? 1.0 : powerUpBuff;
 
-    double totalMultiplier = leaderBuff * highSchoolBuff * moonBaseBuff * charyeokAttackIncrease * powerUpBuff;
+    double totalMultiplier = leaderBuff * highSchoolBuff * moonBaseBuff * charyeokAttackIncrease * powerUpBuff * crestAttackBuff;
 
     // --- Part 3: Damage Type Multipliers ---
     double spiritCritDamage = (_selectedSpirit?.effects['crit_damage'] as num? ?? 0).toDouble();
@@ -233,7 +295,7 @@ class _DamageCalculatorScreenState extends State<DamageCalculatorScreen> {
         _getParser(_divineItemCritDamageController) +
         rebirthCritDmgOption +
         spiritCritDamage +
-        _getParser(_crestCritDamageController) +
+        crestCritDamage +
         passiveCritDamage +
         charyeokCritDamage;
     double critDmgMultiplier = 1 + (critDmgSum / 100);
@@ -252,6 +314,7 @@ class _DamageCalculatorScreenState extends State<DamageCalculatorScreen> {
         spiritSkillDamage +
         charyeokSkillDamage +
         rebirthSkillDmgOption +
+        crestSkillDamage +
         _getParser(_fragmentSkillDamageController);
     double skillDmgMultiplier = 1 + (skillDmgSum / 100);
 
@@ -276,7 +339,6 @@ class _DamageCalculatorScreenState extends State<DamageCalculatorScreen> {
     finalDamage *= skillCoeffMultiplier;
 
     // --- Part 4: Fixed Additional Damage ---
-    // For now, rebirth fixed damage is an input field. This can be updated.
     double rebirthFixedDamage = 0; 
     finalDamage += rebirthFixedDamage + charyeokFixedDamage;
 
@@ -318,6 +380,25 @@ class _DamageCalculatorScreenState extends State<DamageCalculatorScreen> {
             ],
           );
         });
+  }
+
+  Future<void> _showCrestSelectionDialog() async {
+    final result = await showDialog<Map<String, dynamic>>(
+      context: context,
+      builder: (context) {
+        return _CrestSelectionDialog(
+          initialCrest: _selectedCrest ?? crests[0],
+          initialValue: _crestValueController.text,
+        );
+      },
+    );
+
+    if (result != null) {
+      setState(() {
+        _selectedCrest = result['crest'];
+        _crestValueController.text = result['value'];
+      });
+    }
   }
 
   @override
@@ -405,6 +486,48 @@ class _DamageCalculatorScreenState extends State<DamageCalculatorScreen> {
       ),
     );
 
+    Widget crestWidget;
+    if (_selectedCrest != null && _selectedCrest!.type != CrestType.none) {
+        crestWidget = Column(
+            mainAxisSize: MainAxisSize.min,
+            children: [
+                InkWell(
+                    onTap: _showCrestSelectionDialog,
+                    customBorder: const CircleBorder(),
+                    child: Container(
+                        width: 60,
+                        height: 60,
+                        decoration: BoxDecoration(
+                            shape: BoxShape.circle,
+                            color: Colors.white,
+                            border: Border.all(color: Colors.amber, width: 2),
+                        ),
+                        child: Icon(_selectedCrest!.icon, color: Colors.blueGrey, size: 30),
+                    ),
+                ),
+                const SizedBox(height: 4),
+                Text(
+                    _selectedCrest!.name,
+                    style: const TextStyle(color: Colors.white, fontSize: 10, shadows: <Shadow>[Shadow(offset: Offset(1.0, 1.0), blurRadius: 2.0, color: Colors.black,)]),
+                ),
+            ],
+        );
+    } else {
+        crestWidget = InkWell(
+          onTap: _showCrestSelectionDialog,
+          borderRadius: BorderRadius.circular(30),
+          child: Container(
+            padding: const EdgeInsets.symmetric(horizontal: 16, vertical: 10),
+            decoration: BoxDecoration(
+              color: Colors.white,
+              borderRadius: BorderRadius.circular(30),
+              border: Border.all(color: Colors.grey.shade300, width: 2),
+            ),
+            child: const Text('문장 선택'),
+          ),
+        );
+    }
+
     return Scaffold(
       appBar: AppBar(
         title: const Text('데미지 계산기'),
@@ -448,19 +571,20 @@ class _DamageCalculatorScreenState extends State<DamageCalculatorScreen> {
                       },
                     ),
                   ),
-                Padding(
-                  padding: const EdgeInsets.only(top: 8.0, left: 8.0),
-                  child: Align(
-                    alignment: Alignment.topLeft,
-                    child: leaderWidget,
-                  ),
+                Positioned(
+                  top: 8,
+                  left: 8,
+                  child: leaderWidget,
                 ),
-                Padding(
-                  padding: const EdgeInsets.only(top: 8.0, right: 8.0),
-                  child: Align(
-                    alignment: Alignment.topRight,
-                    child: charyeokWidget,
-                  ),
+                Positioned(
+                  top: 68, 
+                  left: 8,
+                  child: crestWidget,
+                ),
+                Positioned(
+                  top: 8,
+                  right: 8,
+                  child: charyeokWidget,
                 ),
               ],
             ),
@@ -503,7 +627,6 @@ class _DamageCalculatorScreenState extends State<DamageCalculatorScreen> {
           children: [
             _buildTextField('추가 공격력', _additionalAttackPowerController),
             _buildTextField('하이스쿨 버프 (%)', _highSchoolBuffController),
-            _buildTextField('달기지 공격력 (%)', _moonBaseBuffController),
             _buildTextField('파워업 (%)', _powerUpController),
           ].map((e) => Padding(padding: const EdgeInsets.symmetric(vertical: 8.0), child: e)).toList(),
         ),
@@ -512,7 +635,6 @@ class _DamageCalculatorScreenState extends State<DamageCalculatorScreen> {
           children: [
             _buildTextField('표기 크뎀', _critDamageController),
             _buildTextField('신기 크뎀', _divineItemCritDamageController),
-            _buildTextField('문장 크뎀', _crestCritDamageController),
           ].map((e) => Padding(padding: const EdgeInsets.symmetric(vertical: 8.0), child: e)).toList(),
         ),
         ExpansionTile(
@@ -676,6 +798,9 @@ extension DropdownDisplay on Object {
                 return '스킬 데미지 증가';
         }
     }
+    if (this is Crest) {
+        return (this as Crest).name;
+    }
     return toString();
   }
 }
@@ -822,5 +947,95 @@ class __CharyeokSelectionDialogState extends State<_CharyeokSelectionDialog> {
   @override
   Widget build(BuildContext context) {
     return _detailedCharyeok == null ? _buildGridView() : _buildDetailView();
+  }
+}
+
+class _CrestSelectionDialog extends StatefulWidget {
+  final Crest initialCrest;
+  final String initialValue;
+
+  const _CrestSelectionDialog({required this.initialCrest, required this.initialValue});
+
+  @override
+  __CrestSelectionDialogState createState() => __CrestSelectionDialogState();
+}
+
+class __CrestSelectionDialogState extends State<_CrestSelectionDialog> {
+  late Crest _selectedCrest;
+  late TextEditingController _valueController;
+
+  @override
+  void initState() {
+    super.initState();
+    _selectedCrest = widget.initialCrest;
+    _valueController = TextEditingController(text: widget.initialValue);
+  }
+
+  @override
+  void dispose() {
+    _valueController.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return AlertDialog(
+      title: const Text('문장 선택'),
+      content: Column(
+        mainAxisSize: MainAxisSize.min,
+        children: [
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            children: crests.where((c) => c.type != CrestType.none).map((crest) {
+              bool isSelected = _selectedCrest.type == crest.type;
+              return InkWell(
+                onTap: () => setState(() => _selectedCrest = crest),
+                child: Container(
+                  padding: const EdgeInsets.all(8),
+                  decoration: BoxDecoration(
+                    shape: BoxShape.circle,
+                    color: isSelected ? Colors.blue.withOpacity(0.2) : Colors.transparent,
+                    border: Border.all(color: isSelected ? Colors.blue : Colors.grey),
+                  ),
+                  child: Column(
+                    children: [
+                      Icon(crest.icon, size: 30),
+                      const SizedBox(height: 4),
+                      Text(crest.name, style: const TextStyle(fontSize: 10)),
+                    ],
+                  ),
+                ),
+              );
+            }).toList(),
+          ),
+          const SizedBox(height: 20),
+          if (_selectedCrest.type != CrestType.none)
+            TextFormField(
+              controller: _valueController,
+              decoration: InputDecoration(
+                labelText: '${_selectedCrest.name} 값',
+                hintText: 'n% 또는 n 입력',
+                border: const OutlineInputBorder(),
+              ),
+              keyboardType: TextInputType.number,
+            ),
+        ],
+      ),
+      actions: [
+        TextButton(
+          child: const Text('취소'),
+          onPressed: () => Navigator.of(context).pop(),
+        ),
+        TextButton(
+          child: const Text('확인'),
+          onPressed: () {
+            Navigator.of(context).pop({
+              'crest': _selectedCrest,
+              'value': _valueController.text,
+            });
+          },
+        ),
+      ],
+    );
   }
 }
