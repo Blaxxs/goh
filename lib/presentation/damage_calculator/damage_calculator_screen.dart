b9a07ed515e8c5f463f02dde796a68e57a4cbf16
Blaxxs <k-yul@naver.com>
2025-09-15 10:37:33 +0900

    Auto-commit on save: 2025-09-15 10:37:33

--- lib/presentation/damage_calculator/damage_calculator_screen.dart
+++ lib/presentation/damage_calculator/damage_calculator_screen.dart
@@ -332,7 +332,7 @@ class _DamageCalculatorScreenState extends State<DamageCalculatorScreen> {
 
 
     // --- Part 2: Multipliers ---
-    final int dalgijiLevel = (SettingsService.instance.stageSettings.dalgijiLevel as int?) ?? 0;
+    final int dalgijiLevel = int.tryParse(SettingsService.instance.stageSettings.dalgijiLevel.toString()) ?? 0;
     final double moonBaseAttackPercent = moonBaseAttackBuffs[dalgijiLevel] ?? 0.0;
     double moonBaseBuff = 1 + (moonBaseAttackPercent / 100);
 
