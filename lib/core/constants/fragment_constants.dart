ead80c0e17bfbaf897475839a5c260aba12c817a
Blaxxs <k-yul@naver.com>
2025-09-19 08:49:14 +0900

    Auto-commit on save: 2025-09-19 08:49:14

--- lib/core/constants/fragment_constants.dart
+++ lib/core/constants/fragment_constants.dart
@@ -14,5 +14,5 @@ const List<Fragment> fragments = [
   Fragment(name: '증폭(스킬)의 파편', imagePath: 'assets/images/fragment/attack.png'),
   Fragment(name: '증폭(미니게임)의 파편', imagePath: 'assets/images/fragment/crit.png'),
   Fragment(name: '증폭(일반)의 파편', imagePath: 'assets/images/fragment/defense.png'),
-    Fragment(name: '증폭(일반)의 파편', imagePath: 'assets/images/fragment/defense.png'),
+  Fragment(name: '증폭(일반)의 파편', imagePath: 'assets/images/fragment/defense.png'),
 ];
