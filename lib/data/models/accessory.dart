48523d594119d21c5c4593a3e81439eaa2d8bb78
Blaxxs <k-yul@naver.com>
2026-01-31 13:15:46 +0900

    Auto-commit on save: 2026-01-31 13:15:45

--- lib/data/models/accessory.dart
+++ lib/data/models/accessory.dart
@@ -20,7 +20,7 @@ class AccessoryOption {
 class Accessory {
   final String id;
   final String name;
-  final String imagePath;
+  final String imageUrl; // 로컬 경로 대신 네트워크 URL 저장
   final String part;
   final String restrictions;
   final List<AccessoryOption> options;
@@ -28,25 +28,24 @@ class Accessory {
   const Accessory({
     required this.id,
     required this.name,
-    required this.imagePath,
+    required this.imageUrl,
     required this.part,
     required this.restrictions,
     required this.options,
   });
 
   factory Accessory.fromJson(Map<String, dynamic> json) {
-    // options가 리스트가 아닌 경우에도 대응
     var list = json['options'] as List? ?? [];
-    List<AccessoryOption> optionsList = list
-        .map((i) => AccessoryOption.fromJson(Map<String, dynamic>.from(i)))
-        .toList();
+    List<AccessoryOption> optionsList = 
+        list.map((i) => AccessoryOption.fromJson(Map<String, dynamic>.from(i))).toList();
 
     return Accessory(
       id: json['id']?.toString() ?? '',
-      name: json['name']?.toString() ?? 'Unknown',
-      imagePath: json['imagePath']?.toString() ?? '',
-      part: json['part']?.toString() ?? '',
-      restrictions: json['restrictions']?.toString() ?? '',
+      name: json['name'] ?? '이름 없음',
+      // Firebase DB에 저장된 'imageUrl' 필드를 읽어옴
+      imageUrl: json['imageUrl'] ?? '', 
+      part: json['part'] ?? '',
+      restrictions: json['restrictions'] ?? '',
       options: optionsList,
     );
   }
