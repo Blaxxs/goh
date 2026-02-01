9cea1fe0fc573c16448974aeb6030c2e57715c92
Blaxxs <k-yul@naver.com>
2026-02-01 15:32:25 +0900

    Auto-commit on save: 2026-02-01 15:32:24

---
+++ deploy.bat
@@ -0,0 +1,22 @@
+@echo OFF
+echo Deploying to GitHub Pages...
+
+REM Change directory to the build output
+pushd build\web
+
+REM Initialize a new git repo and commit the build files
+git init
+git add -A
+git commit -m "Deploy to GitHub Pages"
+
+REM Add the remote origin
+git remote add origin https://github.com/Blaxxs/goh.git
+
+echo Pushing to gh-pages branch...
+REM Force push the current commit to the gh-pages branch on the remote
+git push -f origin HEAD:gh-pages
+
+REM Return to the original directory
+popd
+
+echo Deployment complete.
