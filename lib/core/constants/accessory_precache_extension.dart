decb868a8bdde4b095419eacb40d214ee68470de
Blaxxs <k-yul@naver.com>
2026-02-05 16:51:16 +0900

    Auto-commit on save: 2026-02-05 16:51:16

--- lib/core/constants/accessory_precache_extension.dart
+++ lib/core/constants/accessory_precache_extension.dart
@@ -1,6 +1,7 @@
 // lib/core/constants/accessory_precache_extension.dart
 import 'package:flutter/material.dart';
 import 'package:cached_network_image/cached_network_image.dart';
+import 'package:flutter_cache_manager/flutter_cache_manager.dart';
 import 'accessory_constants.dart';
 import 'package:flutter/foundation.dart';
 
