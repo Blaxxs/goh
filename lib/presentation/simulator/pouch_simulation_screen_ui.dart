d302f4f361b5c2f69e91c774b3ef5b235ce31e89
Blaxxs <k-yul@naver.com>
2026-02-08 00:57:30 +0900

    Auto-commit on save: 2026-02-08 00:57:30

--- lib/presentation/simulator/pouch_simulation_screen_ui.dart
+++ lib/presentation/simulator/pouch_simulation_screen_ui.dart
@@ -316,7 +316,7 @@ class PouchResultDialog extends StatelessWidget {
     final NumberFormat numberFormat = NumberFormat('#,##0');
     final imagePath = _getResultPouchImagePath(pouchType);
     final double amountFontSize =
-      pouchType == PouchType.gold ? 10 : 11;
+      pouchType == PouchType.gold ? 12 : 15;
 
     return AlertDialog(
       title: const Text('주머니 개봉 결과', textAlign: TextAlign.center),
