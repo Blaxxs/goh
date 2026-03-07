a940a4b9420627a64898231dfc51f835049dd293
Blaxxs <k-yul@naver.com>
2026-03-08 03:07:32 +0900

    Auto-commit at 2026-03-08 03:07:32

--- lib/core/widgets/back_to_main_scope.dart
+++ lib/core/widgets/back_to_main_scope.dart
@@ -8,7 +8,7 @@ class BackToMainScope extends StatelessWidget {
   const BackToMainScope({
     super.key,
     required this.child,
-    this.enabled = false,
+    this.enabled = true,
   });
 
   @override
