b8836ae07c8cfedeff71d6dfca42fab1525a84d9
Blaxxs <k-yul@naver.com>
2026-03-28 03:09:35 +0900

    Auto-commit on save

--- autocommit.bat
+++ autocommit.bat
@@ -13,10 +13,8 @@ timeout /t 1 /nobreak > NUL
 REM Add all changes
 git add .
 
-REM Commit with a timestamp
-FOR /F "tokens=2 delims==" %%I IN ('wmic os get localdatetime /value') DO SET "timestamp=%%I"
-SET "commit_message=Auto-commit at %timestamp:~0,4%-%timestamp:~4,2%-%timestamp:~6,2% %timestamp:~8,2%:%timestamp:~10,2%:%timestamp:~12,2%"
-git commit -m "%commit_message%"
+REM Commit with a simple message
+git commit -m "Auto-commit on save"
 
 REM Push to the remote repository
 git push
