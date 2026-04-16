2de3d51eaab033a9e5978bc6b6ee3ecb7a761592
Your Name <your-email@example.com>
2026-04-17 02:11:30 +0900

    Deploy: 웹 빌드

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"cf56914b326edb0ccb123ffdc60f00060bd513
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "3768280105"
+    serviceWorkerVersion: "1797478333"
   }
 });
