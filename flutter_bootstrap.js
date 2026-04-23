a460b1050446ebedaa520a0667d2658182c90537
Your Name <your-email@example.com>
2026-04-23 14:19:36 +0900

    Deploy to GitHub Pages

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"cf56914b326edb0ccb123ffdc60f00060bd513
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "927928377"
+    serviceWorkerVersion: "1306318651"
   }
 });
